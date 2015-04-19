4c7e79deacd4335a201d61921cad470a1a16839f
BostonA <BostonWAbrams@gmail.com>
2015-04-19 16:08:18 -0700

    Added .py File

---
+++ TreaureHuntProgram.py
@@ -0,0 +1,12 @@
+print "Welcome to the computer part of this hunt"
+while True:
+    code = raw_input("Enter in the code >>>")
+    if code == "1254784":
+        print "Go this web adress http://tinyurl.com/TreasureHuntA"
+    if code == "4902672":
+        print "Go this web adress http://tinyurl.com/TreasureHuntB"
+    if code == "8372028":
+        print "Go this web adress http://tinyurl.com/TreasureHuntC"
+    if code == "8032476":
+        print "Go this web adress http://tinyurl.com/TreasureHuntD"
+
